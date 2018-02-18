5f3af56b446892712efcfb2dc3e073caf5eaca2e
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2018-02-18 19:07:49 +0300

    Solved

--- src/Expr.ml
+++ src/Expr.ml
@@ -36,13 +36,13 @@ let update x v s = fun y -> if x = y then v else s y
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
+(* let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
-
+ *)
 (* Expression evaluator
 
      val eval : state -> expr -> int
@@ -50,5 +50,30 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
+
+let asboolean s = s <> 0
+
+let asinteger b = if b then 1 else 0
+
+let rec eval s e = match e with
+  | Const c -> c
+  | Var v -> s v
+  | Binop (o, el, er) ->
+    let lr = eval s el in
+    let rr = eval s er in
+    match o with
+      | "!!" -> asinteger (asboolean lr || asboolean rr)
+      | "&&" -> asinteger (asboolean lr && asboolean rr)
+      | "==" -> asinteger (lr = rr) 
+      | "!=" -> asinteger (lr <> rr) 
+      | "<=" -> asinteger (lr <= rr) 
+      | ">=" -> asinteger (lr >= rr)
+      | "<" -> asinteger (lr < rr)
+      | ">" -> asinteger (lr > rr)
+      | "+" -> lr + rr 
+      | "-" -> lr - rr
+      | "*" -> lr * rr 
+      | "/" -> lr / rr 
+      | "%" -> lr mod rr     
+      | _ -> failwith "unsupported binary operator"
                     
